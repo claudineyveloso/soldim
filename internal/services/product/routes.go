0b31c022c50750d2f6b51fbcbbff288cd8c59b16
Claudiney Veloso <claudineyveloso@gmail.com>
2024-07-02 19:41:38 -0300

    refresh token

--- internal/services/product/routes.go
+++ internal/services/product/routes.go
@@ -23,7 +23,7 @@ func RegisterRoutes(router *mux.Router) {
 }
 
 func handleGetProduct(w http.ResponseWriter, r *http.Request) {
-	bearerToken := "2f1bf860f6c12bcb5abbd1f7b36d64e3152b6829" // r.Header.Get("Authorization")
+	bearerToken := "25cfbdc036d36979cfecde53a8516ae7af9ca271" // r.Header.Get("Authorization")
 
 	pageStr := r.URL.Query().Get("page")
 	limitStr := r.URL.Query().Get("limit")
@@ -90,7 +90,7 @@ func handleCreateProduct(w http.ResponseWriter, r *http.Request) {
 	// Fecha o corpo da requisição após o processamento
 	defer r.Body.Close()
 
-	bearerToken := "2f1bf860f6c12bcb5abbd1f7b36d64e3152b6829" // r.Header.Get("Authorization")
+	bearerToken := "25cfbdc036d36979cfecde53a8516ae7af9ca271" // r.Header.Get("Authorization")
 	// Chama a função para criar o produto no Bling
 	err := bling.CreateProductInBling(bearerToken, newProduct)
 	if err != nil {
@@ -164,7 +164,7 @@ func handleDeleteProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bearerToken := "2f1bf860f6c12bcb5abbd1f7b36d64e3152b6829" // r.Header.Get("Authorization")
+	bearerToken := "25cfbdc036d36979cfecde53a8516ae7af9ca271" // r.Header.Get("Authorization")
 	// Chama a função para deletar o produto no Bling
 	err = bling.DeleteProductInBling(bearerToken, productID)
 	if err != nil {
@@ -196,7 +196,7 @@ func handleGetProductId(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	bearerToken := "2f1bf860f6c12bcb5abbd1f7b36d64e3152b6829" // r.Header.Get("Authorization")
+	bearerToken := "25cfbdc036d36979cfecde53a8516ae7af9ca271" // r.Header.Get("Authorization")
 	// Chama a função para obter os detalhes do produto no Bling
 	product, err := bling.GetProductIDInBling(bearerToken, productID)
 	if err != nil {
--- internal/services/refresh_token/routes.go
+++ internal/services/refresh_token/routes.go
@@ -54,7 +54,7 @@ func RegisterRoutes(router *mux.Router) {
 func handlePostRefreshToken(w http.ResponseWriter, r *http.Request) {
 	username := "11e56de94a8dc983459367236b79608cd941dda6"
 	password := "26ef0f168a6c9fc7618cafacbead208a9cb4a9d2492c1f33ac4a8ccfb2c3"
-	refreshToken := "a7d7a56206069b779dc3e7be6bc2639521365dde"
+	refreshToken := "967ef1c2dc3a6a691ed0e8ff53a7372f3f3aac89"
 
 	fmt.Println("Enviando requisição para gerar novo token de atualização...")
 
