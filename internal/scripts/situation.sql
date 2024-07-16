099db19e7a1a79cb7289e98bd69368a917083285
Claudiney Veloso <claudineyveloso@gmail.com>
2024-07-16 17:25:10 -0300

    situation sql

---
+++ internal/scripts/situation.sql
@@ -0,0 +1,28 @@
+-- name: CreateSituation :exec
+INSERT INTO situations (id, descricao, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $5);
+
+-- name: GetSituations :many
+SELECT id,
+        descricao,
+        created_at,
+        updated_at
+FROM situations
+
+-- name: GetSituation :one
+SELECT id,
+        descricao,
+        created_at,
+        updated_at
+FROM situations
+WHERE situations.id = $1;
+
+-- name: GetSituationByDescroption :one
+SELECT id,
+        descricao,
+        created_at,
+        updated_at
+FROM situations
+WHERE situations.descricao = $1;
+
+
