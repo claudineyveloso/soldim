6056efc6aee352956d3e58dd974d78bb29ca2631
Claudiney Veloso <claudineyveloso@gmail.com>
2024-07-20 11:48:09 -0300

    created store products sales orders

---
+++ internal/services/products_sales_order/store.go
@@ -0,0 +1,135 @@
+package productssalesorder
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/claudineyveloso/soldim.git/internal/db"
+	"github.com/claudineyveloso/soldim.git/internal/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) CreateProductsSalesOrder(productsalesorder types.ProductSalesOrder) error {
+	queries := db.New(s.db)
+	ctx := context.Background()
+
+	now := time.Now()
+	productsalesorder.CreatedAt = now
+	productsalesorder.UpdatedAt = now
+	createProductSalesOrderParams := db.CreateProductSalesOrderParams{
+		SalesOrderID: productsalesorder.SalesOrderID,
+		ProductID:    productsalesorder.ProductID,
+		Quantidade:   productsalesorder.Quantidade,
+		CreatedAt:    productsalesorder.CreatedAt,
+		UpdatedAt:    productsalesorder.UpdatedAt,
+	}
+
+	fmt.Println("Criando um Pedido de Vendas...", createProductSalesOrderParams)
+
+	if err := queries.CreateProductSalesOrder(ctx, createProductSalesOrderParams); err != nil {
+		fmt.Println("Erro ao criar um Pedido de Vendas:", err)
+		return err
+	}
+	return nil
+}
+
+func (s *Store) GetProductSalesOrders() ([]*types.ProductSalesOrder, error) {
+	queries := db.New(s.db)
+	ctx := context.Background()
+
+	dbProductsSalesOrders, err := queries.GetProductSalesOrder(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return convertDBProductsSalesOrders(dbProductsSalesOrders), nil
+}
+
+func (s *Store) GetProductSalesOrdersBySupplierID(supplierID int64) ([]*types.ProductSalesOrder, error) {
+	queries := db.New(s.db)
+	ctx := context.Background()
+
+	dbSalesOrders, err := queries.GetProductSalesOrderBySupplierID(ctx, supplierID)
+	if err != nil {
+		return nil, err
+	}
+
+	return convertDBProductsSalesOrders(dbSalesOrders), nil
+}
+
+func convertDBProductsSalesOrderToProductsSalesOrder(row interface{}) *types.ProductSalesOrder {
+	switch r := row.(type) {
+	case db.GetProductSalesOrderRow:
+		return &types.ProductSalesOrder{
+			ID:             types.ToInt64(r.ID),
+			SalesOrderID:   r.SalesOrderID,
+			ProductID:      r.ProductID,
+			Quantidade:     r.Quantidade,
+			CreatedAt:      r.CreatedAt,
+			UpdatedAt:      r.UpdatedAt,
+			Numero:         types.ToInt32(r.Numero),
+			Numeroloja:     types.ToString(r.Numeroloja),
+			Data:           types.ToTime(r.Data),
+			Datasaida:      types.ToTime(r.Datasaida),
+			Dataprevista:   types.ToTime(r.Dataprevista),
+			Totalprodutos:  types.ToFloat64(r.Totalprodutos),
+			Totaldescontos: types.ToFloat64(r.Totaldescontos),
+			SituationID:    types.ToInt64(r.SituationID),
+			StoreID:        types.ToInt64(r.StoreID),
+			SupplierID:     types.ToInt64(r.SupplierID),
+			Nome:           types.ToString(r.Nome),
+			Codigo:         types.ToString(r.Codigo),
+			Preco:          types.ToFloat64(r.Preco),
+		}
+	case db.GetProductSalesOrderBySupplierIDRow:
+		return &types.ProductSalesOrder{
+			ID:             types.ToInt64(r.ID),
+			SalesOrderID:   r.SalesOrderID,
+			ProductID:      r.ProductID,
+			Quantidade:     r.Quantidade,
+			CreatedAt:      r.CreatedAt,
+			UpdatedAt:      r.UpdatedAt,
+			Numero:         types.ToInt32(r.Numero),
+			Numeroloja:     types.ToString(r.Numeroloja),
+			Data:           types.ToTime(r.Data),
+			Datasaida:      types.ToTime(r.Datasaida),
+			Dataprevista:   types.ToTime(r.Dataprevista),
+			Totalprodutos:  types.ToFloat64(r.Totalprodutos),
+			Totaldescontos: types.ToFloat64(r.Totaldescontos),
+			SituationID:    types.ToInt64(r.SituationID),
+			StoreID:        types.ToInt64(r.StoreID),
+			SupplierID:     types.ToInt64(r.SupplierID),
+			Nome:           types.ToString(r.Nome),
+			Codigo:         types.ToString(r.Codigo),
+			Preco:          types.ToFloat64(r.Preco),
+		}
+	default:
+		return nil
+	}
+}
+
+func convertDBProductsSalesOrders(rows interface{}) []*types.ProductSalesOrder {
+	var productssalesorders []*types.ProductSalesOrder
+	switch rs := rows.(type) {
+	case []db.GetProductSalesOrderRow:
+		for _, r := range rs {
+			productsalesorder := convertDBProductsSalesOrderToProductsSalesOrder(r)
+			productssalesorders = append(productssalesorders, productsalesorder)
+		}
+	case []db.GetProductSalesOrderBySupplierIDRow:
+		for _, r := range rs {
+			productsalesorder := convertDBProductsSalesOrderToProductsSalesOrder(r)
+			productssalesorders = append(productssalesorders, productsalesorder)
+		}
+	}
+	return productssalesorders
+}
--- internal/types/products_sales_order.go
+++ internal/types/products_sales_order.go
@@ -1,13 +1,65 @@
 package types
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 type ProductSalesOrder struct {
-	SalesOrderID int64     `json:"sales_order_id"`
-	ProductID    int64     `json:"product_id"`
-	Quantidade   int32     `json:"quantidade"`
-	CreatedAt    time.Time `json:"created_at"`
-	UpdatedAt    time.Time `json:"updated_at"`
+	ID             int64
+	SalesOrderID   int64
+	ProductID      int64
+	Quantidade     int32
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Numero         int32
+	Numeroloja     string
+	Data           time.Time
+	Datasaida      time.Time
+	Dataprevista   time.Time
+	Totalprodutos  float64
+	Totaldescontos float64
+	SituationID    int64
+	StoreID        int64
+	SupplierID     int64
+	Nome           string
+	Codigo         string
+	Preco          float64
+}
+
+func ToInt64(n sql.NullInt64) int64 {
+	if n.Valid {
+		return n.Int64
+	}
+	return 0
+}
+
+func ToInt32(n sql.NullInt32) int32 {
+	if n.Valid {
+		return n.Int32
+	}
+	return 0
+}
+
+func ToString(s sql.NullString) string {
+	if s.Valid {
+		return s.String
+	}
+	return ""
+}
+
+func ToFloat64(f sql.NullFloat64) float64 {
+	if f.Valid {
+		return f.Float64
+	}
+	return 0.0
+}
+
+func ToTime(t sql.NullTime) time.Time {
+	if t.Valid {
+		return t.Time
+	}
+	return time.Time{}
 }
 
 type ProductSalesOrderStore interface {
