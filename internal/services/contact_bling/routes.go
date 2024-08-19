3a2e1ee3bd2381f55a75d537446cae7e50f33dac
Claudiney Veloso <claudineyveloso@gmail.com>
2024-08-19 19:40:05 -0300

    b

--- internal/services/contact_bling/routes.go
+++ internal/services/contact_bling/routes.go
@@ -26,8 +26,7 @@ func handleImportBlingContactsToSoldim(w http.ResponseWriter, r *http.Request) {
 	channels, err := bling.GetContactsFromBling(bearerToken)
 	if err != nil {
 
-		http.Error(w, fmt.Sprintf("EValor de bearerToken: %s", bearerToken), http.StatusInternalServerError)
-		// http.Error(w, fmt.Sprintf("Error getting contacts from Bling: %v", err), http.StatusInternalServerError)
+		http.Error(w, fmt.Sprintf("Errorff getting contacts from Bling: %v", err), http.StatusInternalServerError)
 		return
 	}
 
