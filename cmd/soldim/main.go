1c229a21a2a1a027beef587607cd78fa153b2b07
Claudiney Veloso <claudineyveloso@gmail.com>
2024-08-19 17:46:58 -0300

    t

--- cmd/soldim/main.go
+++ cmd/soldim/main.go
@@ -2,81 +2,26 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
-	"github.com/claudineyveloso/soldim.git/cmd/api"
-	"github.com/claudineyveloso/soldim.git/cmd/db"
-	"github.com/claudineyveloso/soldim.git/internal/configs"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	cfg := configs.Config{
-		Host:       configs.Envs.Host,
-		Port:       configs.Envs.Port,
-		DBUser:     configs.Envs.DBUser,
-		DBPassword: configs.Envs.DBPassword,
-		DBName:     configs.Envs.DBName,
-	}
-	db, err := db.NewPostgresSQLStorage(cfg)
-	if err != nil {
-		log.Fatal(err)
-	}
+    connStr := "host=c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com port=5432 user=u8v8gfju2nbfqn password=p009827e7e08f28b44b9ba56751f82543345f445a718fb44624dd5b653e0238bd dbname=ddcvr5rele9132 sslmode=require"
 
-	initStorage(db)
+    log.Println("About to connect to the database with the following connection string")
+    log.Println("Connection string:", connStr)
 
-	server := api.NewAPIServer(fmt.Sprintf(":%s", configs.Envs.Port), db)
-	if err := server.Run(); err != nil {
-		log.Fatal(err)
-	}
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal("Failed to connect to the database:", err)
+    }
+
+    log.Println("Connected to the database successfully!")
 }
-
-func initStorage(db *sql.DB) {
-	err := db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("DB: Successfully connected!")
-}
-
-// ##################################################################
-
-// package main
-//
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-//
-// 	"github.com/claudineyveloso/soldim.git/internal/crawler"
-// 	"github.com/gorilla/mux"
-// )
-//
-// func main() {
-// 	r := mux.NewRouter()
-// 	r.HandleFunc("/crawl", handleCrawl).Methods("GET")
-//
-// 	fmt.Println("Server started at :8080")
-// 	log.Fatal(http.ListenAndServe(":8080", r))
-// }
-//
-// func handleCrawl(w http.ResponseWriter, r *http.Request) {
-// 	query := r.URL.Query().Get("query")
-// 	if query == "" {
-// 		http.Error(w, "query parameter is required", http.StatusBadRequest)
-// 		return
-// 	}
-//
-// 	produtos, err := crawler.CrawlGoogle(query)
-// 	if err != nil {
-// 		http.Error(w, fmt.Sprintf("error crawling data: %v", err), http.StatusInternalServerError)
-// 		return
-// 	}
-//
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(produtos)
-//
-// 	// Log the total number of products collected
-// 	log.Printf("Total de produtos coletados: %d", len(produtos))
-// }
