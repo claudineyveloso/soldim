500d589f7287c32ae776b564841aa758ce7ab4c8
Claudiney Veloso <claudineyveloso@gmail.com>
2024-08-21 20:48:28 -0300

    removed comment

--- cmd/db/db.go
+++ cmd/db/db.go
@@ -50,7 +50,6 @@ func NewPostgresSQLStorage(cfg configs.Config) (*sql.DB, error) {
 		"password=%s dbname=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.DBUser, cfg.DBPassword, cfg.DBName, sslMode)
 
-	// connStr := "host=c3gtj1dt5vh48j.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com port=5432 user=u8v8gfju2nbfqn password=p009827e7e08f28b44b9ba56751f82543345f445a718fb44624dd5b653e0238bd dbname=ddcvr5rele9132 sslmode=require"
 	dbConn, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
