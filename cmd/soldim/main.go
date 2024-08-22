fc42d02d85d32bed49169e923838e217a4cb3030
Claudiney Veloso <claudineyveloso@gmail.com>
2024-08-22 11:31:00 -0300

    updated main.com

--- cmd/soldim/main.go
+++ cmd/soldim/main.go
@@ -1,69 +1,68 @@
-// package main
-
-// import (
-// 	"database/sql"
-// 	"fmt"
-// 	"log"
-
-// 	"github.com/claudineyveloso/soldim.git/cmd/api"
-// 	"github.com/claudineyveloso/soldim.git/cmd/db"
-// 	"github.com/claudineyveloso/soldim.git/internal/configs"
-// )
-
-// func main() {
-// 	cfg := configs.Config{
-// 		Host:       configs.Envs.Host,
-// 		Port:       configs.Envs.Port,
-// 		DBUser:     configs.Envs.DBUser,
-// 		DBPassword: configs.Envs.DBPassword,
-// 		DBName:     configs.Envs.DBName,
-// 	}
-
-// 	db, err := db.NewPostgresSQLStorage(cfg)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	initStorage(db)
-
-// 	server := api.NewAPIServer(fmt.Sprintf(":%s", configs.Envs.Port), db)
-// 	if err := server.Run(); err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
-
-// func initStorage(db *sql.DB) {
-// 	err := db.Ping()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Println("DB: Successfully connected!")
-// }
-//
-
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/claudineyveloso/soldim.git/cmd/api"
+	"github.com/claudineyveloso/soldim.git/cmd/db"
+	"github.com/claudineyveloso/soldim.git/internal/configs"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, world!")
-}
-
 func main() {
-    http.HandleFunc("/", handler)
+	cfg := configs.Config{
+		Host:       configs.Envs.Host,
+		Port:       configs.Envs.Port,
+		DBUser:     configs.Envs.DBUser,
+		DBPassword: configs.Envs.DBPassword,
+		DBName:     configs.Envs.DBName,
+	}
 
-    // Use a porta fornecida pela variável de ambiente PORT
-    port := os.Getenv("PORT")
-    if port == "" {
-      port = "8080"
-      log.Fatal("Porta não definida na variável de ambiente PORT")
-    }
+	db, err := db.NewPostgresSQLStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    log.Printf("Servidor rodando em http://localhost:%s/", port)
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+	initStorage(db)
+
+	server := api.NewAPIServer(fmt.Sprintf(":%s", configs.Envs.Port), db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: Successfully connected!")
+}
+
+// package main
+//
+// import (
+//     "fmt"
+//     "log"
+//     "net/http"
+//     "os"
+// )
+//
+// func handler(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w, "Hello, world!")
+// }
+//
+// func main() {
+//     http.HandleFunc("/", handler)
+//
+//     // Use a porta fornecida pela variável de ambiente PORT
+//     port := os.Getenv("PORT")
+//     if port == "" {
+//       port = "8080"
+//       log.Fatal("Porta não definida na variável de ambiente PORT")
+//     }
+//
+//     log.Printf("Servidor rodando em http://localhost:%s/", port)
+//     log.Fatal(http.ListenAndServe(":"+port, nil))
+// }
