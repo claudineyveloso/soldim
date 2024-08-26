3d8b9afa7e352280662aeb8628769a608d34e47d
Claudiney Veloso <claudineyveloso@gmail.com>
2024-08-26 18:39:31 -0300

    refresh token

--- internal/services/refresh_token/routes.go
+++ internal/services/refresh_token/routes.go
@@ -54,7 +54,7 @@ func RegisterRoutes(router *mux.Router) {
 func handlePostRefreshToken(w http.ResponseWriter, r *http.Request) {
 	username := "11e56de94a8dc983459367236b79608cd941dda6"
 	password := "26ef0f168a6c9fc7618cafacbead208a9cb4a9d2492c1f33ac4a8ccfb2c3"
-	refreshToken := "a3347a2c2a99900bfce72c2041897fb1b2205517"
+	refreshToken := "c35db966d22a77165c4d918fe5dd5493e34e8192"
 
 	fmt.Println("Enviando requisição para gerar novo token de atualização...")
 
