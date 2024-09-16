b401c75788e0f0ba724f93c7b845d5723bfe3168
Claudiney Veloso <claudineyveloso@gmail.com>
2024-09-16 20:09:46 -0300

    update soldim-api

--- internal/crawler/google.go
+++ internal/crawler/google.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -63,6 +64,214 @@ func CrawlGoogle(query string) ([]Produto, error) {
 		return nil, fmt.Errorf("falha ao extrair HTML: %v", err)
 	}
 
+	// Parsear o HTML usando goquery
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		log.Println("Falha ao parsear HTML:", err)
+		return nil, fmt.Errorf("falha ao parsear HTML: %v", err)
+	}
+
+	// Coletar dados de até 10 produtos
+	var produtos []Produto
+	doc.Find("div.sh-dgr__grid-result").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		if i >= 10 {
+			return false // Interrompe a iteração após 10 produtos
+		}
+
+		nome := s.Find("h3.tAxDx").Text()
+		link, _ := s.Find("a.xCpuod").Attr("href")
+		imagemURL, _ := s.Find("img").Attr("src")
+		precoStr := s.Find("span.a8Pemb").Text()
+
+		// Capturar o texto visível do fornecedor usando JavaScript
+		var fornecedor string
+		err := chromedp.Run(ctx, chromedp.Evaluate(`document.querySelector("div.aULzUe").innerText`, &fornecedor))
+		if err != nil {
+			log.Println("Falha ao extrair fornecedor:", err)
+			fornecedor = "Desconhecido"
+		}
+
+		// Verificar se o preço foi encontrado
+		if precoStr == "" {
+			log.Println("Preço não encontrado para o produto:", nome)
+			precoStr = "0" // Valor padrão caso o preço não seja encontrado
+		}
+
+		// Formatar o preço
+		precoStr = strings.TrimSpace(precoStr)                // Remove espaços extras
+		precoStr = strings.ReplaceAll(precoStr, "R$", "")     // Remove símbolo de moeda
+		precoStr = strings.ReplaceAll(precoStr, ".", "")      // Remove pontos (milhares)
+		precoStr = strings.ReplaceAll(precoStr, ",", ".")     // Substitui vírgulas por pontos (decimais)
+		precoStr = strings.ReplaceAll(precoStr, "\u00a0", "") // Remove espaços não separáveis
+
+		// Converter preço de string para float64
+		preco, err := strconv.ParseFloat(precoStr, 64)
+		if err != nil {
+			log.Printf("Erro ao converter preço '%s' para float64: %v", precoStr, err)
+			preco = 0 // Valor padrão caso a conversão falhe
+		}
+
+		produto := Produto{
+			Description: nome,
+			Link:        "https://www.google.com" + link,
+			ImageURL:    imagemURL,
+			Price:       preco,
+			Source:      fornecedor,
+		}
+
+		produtos = append(produtos, produto)
+
+		return true
+	})
+
+	// Exibir os produtos coletados
+	for _, produto := range produtos {
+		log.Printf("Nome: %s\nLink: %s\nImagem: %s\nPreço: %.2f\nFornecedor: %s\n\n",
+			produto.Description, produto.Link, produto.ImageURL, produto.Price, produto.Source)
+	}
+
+	// Retornar a lista de produtos coletados
+	return produtos, nil
+}
+
+func CrawlGoogle000(query string) ([]Produto, error) {
+	// Configurar opções para o Chromium
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.Flag("no-sandbox", true),            // Necessário para Heroku
+		chromedp.Flag("disable-dev-shm-usage", true), // Pode ajudar a evitar problemas de memória
+		chromedp.Flag("disable-gpu", true),           // O Heroku não precisa de GPU
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
+
+	// Criar um novo contexto do Chromium com as opções
+	ctx, cancel = chromedp.NewExecAllocator(ctx, opts...)
+	defer cancel()
+
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+
+	// Codificar a query string para ser usada na URL
+	encodedQuery := url.QueryEscape(query)
+	startURL := fmt.Sprintf("https://www.google.com/search?q=%s&tbm=shop", encodedQuery)
+	log.Println("Iniciando visita:", startURL)
+
+	// Navegar até a URL inicial
+	err := chromedp.Run(ctx, chromedp.Navigate(startURL))
+	if err != nil {
+		log.Println("Falha ao iniciar a visita:", err)
+		return nil, fmt.Errorf("falha ao iniciar a visita: %v", err)
+	}
+
+	// Extrair o HTML da página
+	var htmlContent string
+	err = chromedp.Run(ctx, chromedp.OuterHTML(`html`, &htmlContent, chromedp.ByQuery))
+	if err != nil {
+		log.Println("Falha ao extrair HTML:", err)
+		return nil, fmt.Errorf("falha ao extrair HTML: %v", err)
+	}
+
+	// Parsear o HTML usando goquery
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
+	if err != nil {
+		log.Println("Falha ao parsear HTML:", err)
+		return nil, fmt.Errorf("falha ao parsear HTML: %v", err)
+	}
+
+	// Coletar dados de até 10 produtos
+	var produtos []Produto
+	doc.Find("div.sh-dgr__grid-result").EachWithBreak(func(i int, s *goquery.Selection) bool {
+		if i >= 10 {
+			return false // Interrompe a iteração após 10 produtos
+		}
+
+		nome := s.Find("h3.tAxDx").Text()
+		link, _ := s.Find("a.xCpuod").Attr("href")
+		imagemURL, _ := s.Find("img").Attr("src")
+		precoStr := s.Find("span.a8Pemb").Text()
+
+		// Capturar o texto visível do fornecedor usando JavaScript
+		var fornecedor string
+		err := chromedp.Run(ctx, chromedp.Evaluate(`document.querySelector("div.aULzUe").innerText`, &fornecedor))
+		if err != nil {
+			log.Println("Falha ao extrair fornecedor:", err)
+			fornecedor = "Desconhecido"
+		}
+
+		// Converter preço de string para float64
+		precoStr = strings.ReplaceAll(precoStr, "R$", "") // Remove simbolo de moeda
+		precoStr = strings.ReplaceAll(precoStr, ".", "")  // Remove pontos
+		precoStr = strings.ReplaceAll(precoStr, ",", ".") // Substitui vírgulas por pontos
+		preco, err := strconv.ParseFloat(precoStr, 64)
+		if err != nil {
+			log.Printf("Erro ao converter preço '%s' para float64: %v", precoStr, err)
+			preco = 0 // Valor padrão caso a conversão falhe
+		}
+
+		produto := Produto{
+			Description: nome,
+			Link:        "https://www.google.com" + link,
+			ImageURL:    imagemURL,
+			Price:       preco,
+			Source:      fornecedor,
+		}
+
+		produtos = append(produtos, produto)
+
+		return true
+	})
+
+	// Exibir os produtos coletados
+	for _, produto := range produtos {
+		log.Printf("Nome: %s\nLink: %s\nImagem: %s\nPreço: %.2f\nFornecedor: %s\n\n",
+			produto.Description, produto.Link, produto.ImageURL, produto.Price, produto.Source)
+	}
+
+	// Retornar a lista de produtos coletados
+	return produtos, nil
+}
+
+func CrawlGoogle11111(query string) ([]Produto, error) {
+	// Configurar opções para o Chromium
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.Flag("no-sandbox", true),            // Necessário para Heroku
+		chromedp.Flag("disable-dev-shm-usage", true), // Pode ajudar a evitar problemas de memória
+		chromedp.Flag("disable-gpu", true),           // O Heroku não precisa de GPU
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer cancel()
+
+	// Criar um novo contexto do Chromium com as opções
+	ctx, cancel = chromedp.NewExecAllocator(ctx, opts...)
+	defer cancel()
+
+	ctx, cancel = chromedp.NewContext(ctx)
+	defer cancel()
+
+	// Codificar a query string para ser usada na URL
+	encodedQuery := url.QueryEscape(query)
+	startURL := fmt.Sprintf("https://www.google.com/search?q=%s&tbm=shop", encodedQuery)
+	log.Println("Iniciando visita:", startURL)
+
+	// Navegar até a URL inicial
+	err := chromedp.Run(ctx, chromedp.Navigate(startURL))
+	if err != nil {
+		log.Println("Falha ao iniciar a visita:", err)
+		return nil, fmt.Errorf("falha ao iniciar a visita: %v", err)
+	}
+
+	// Extrair o HTML da página
+	var htmlContent string
+	err = chromedp.Run(ctx, chromedp.OuterHTML(`html`, &htmlContent, chromedp.ByQuery))
+	if err != nil {
+		log.Println("Falha ao extrair HTML:", err)
+		return nil, fmt.Errorf("falha ao extrair HTML: %v", err)
+	}
+
 	// Log do tamanho do HTML extraído
 	log.Println("Tamanho do HTML extraído:", len(htmlContent))
 
@@ -70,6 +279,13 @@ func CrawlGoogle(query string) ([]Produto, error) {
 	log.Println("HTML da página extraído:")
 	log.Println(htmlContent)
 
+	filePath := "html_output.txt"
+	err = os.WriteFile(filePath, []byte(htmlContent), 0644)
+	if err != nil {
+		log.Println("Falha ao gravar HTML em arquivo:", err)
+	} else {
+		log.Println("HTML salvo com sucesso em", filePath)
+	}
 	// Retornar uma lista vazia de produtos
 	return []Produto{}, nil
 }
