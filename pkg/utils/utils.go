bd17e469c130aea7f0847b6433f74568f33dd762
Claudiney Veloso <claudineyveloso@gmail.com>
2024-09-23 00:04:11 -0300

    update

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -37,8 +37,8 @@ func GetBaseURL() string {
 	env := os.Getenv("ENVIRONMENT")
 
 	if env == "prod" {
-		return "https://soldim-api-344942c665db.herokuapp.com"
-		// return "http://3.80.249.138:8888"
+		// return "https://soldim-api-344942c665db.herokuapp.com"
+		return "http://3.80.249.138:8888"
 	}
 	return "http://localhost:8080"
 }
